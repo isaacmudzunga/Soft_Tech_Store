ba32d2d1ce3f423f1c407f997474356a226fc125
ismudzu022 <ismudzu022@student.wethinkcode.co.za>
2023-10-02 12:11:35 +0200

    Added a react template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
+    <Header></Header>
+    <main>
       <h1>Welcome To SoftTech Store</h1>
+    </main>
+    <Footer></Footer>
     </>
   );
 }
