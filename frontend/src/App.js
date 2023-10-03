d397fe2f9441531d3b61e4edd0f4962484f71614
ismudzu022 <ismudzu022@student.wethinkcode.co.za>
2023-10-03 13:13:08 +0200

    Stuck on the Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './Screens/HomeScreen'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
-      <Header/>
+    <Router>
+      <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen/>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen/>} />
+          </Routes>
         </Container>
       </main>
-      <Footer/>
-    </>
-  )
-}
+      <Footer />
+    </Router>
+  );
+};
 
-export default App
+export default App;
